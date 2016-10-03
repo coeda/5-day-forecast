2b29cfe0582f259b349d44815d6fd72615962f41
Casey Oeda <coeda@live.com>
2016-10-03 13:53:09 -1000

    created project, hid api key

---
+++ public/js/client.js
@@ -0,0 +1,38 @@
+let getWeatherData = document.querySelector('#weather');
+let container = document.querySelector('#container');
+
+getWeatherData.addEventListener('click', function(event) {
+
+      let weather = [];
+      this.container = document.getElementById('container');
+      let oReq = new XMLHttpRequest();
+        oReq.addEventListener('progress', function(){
+        });
+        oReq.addEventListener('error', function(){
+          console.log('ERROR :<');
+        });
+        oReq.addEventListener('load', function(){
+          let parsedWeatherData = JSON.parse(this.responseText);
+          weather = parsedWeatherData.list;
+          console.log(parsedWeatherData.city.name);
+          const view = document.createElement('div');
+          const items = weather.map(newWeather => {
+            let item = document.createElement('div');
+            let title = document.createTextNode(newWeather.city);
+            item.appendChild(title);
+            return item;
+          });
+          console.log(weather[1]);
+          items.forEach(view.appendChild.bind(view));
+          updateContainer(weather.city, container);
+        });
+      function updateContainer(data, container){
+        container.innerHTML = '';
+        container.innerHTML = data;
+      }
+      console.log(key.apiKey);
+      oReq.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?q=Honolulu,US&appid=' + key.apiKey);
+      oReq.setRequestHeader('Accept', 'application/json');
+      oReq.send();
+
+  });
